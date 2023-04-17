d1a40a427d5c2f3a7ae49ca085aa81dab938c0a0
SeangLeng <seangleng507@gmail.com>
2023-04-17 21:33:29 +0700

    done

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Cards from './component/Cards';
+import Nav from './component/Nav';
+import Bdy from './component/Body';
+import About from './component/About';
+import Footer from './component/Footer';
+function App() {
+  return (
+    <>
+      <Nav/>
+      <Bdy/>
+      <Cards/>
+      <About/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App;
